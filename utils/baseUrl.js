74ee5e242006cb4b98db24ed8840bd094a0b3078
darkillusior <mysteryuniverse12@gmail.com>
2023-02-07 00:00:41 +0530

    d

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:9001"
-   :"https://pink-plain-cockroach.cyclic.app"
+   :"https://lively-cow-polo-shirt.cyclic.app/"
 
 module.exports = baseUrl;
