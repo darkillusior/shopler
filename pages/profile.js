49cfc432374b272f7e0dad5b826e0938d697be21
g4urav <gaurav.gb.bahuguna@gmail.com>
2023-03-08 20:10:57 +0530

    Co-authored-by: darkillusior <darkillusior@users.noreply.github.com>

--- pages/profile.js
+++ pages/profile.js
@@ -40,7 +40,7 @@ function profile({user}) {
                                     <span class="text-black ml-3 text-xs">Time</span>
                             </a>
 
-                            <a href="#" class="w-full border-t border-gray-100 text-gray-600 py-4 pl-6 pr-3 w-full block hover:bg-gray-100 transition duration-150">
+                            <a href="#" class="w-full border-t border-gray-100 text-gray-600 py-4 pl-6 pr-3  block hover:bg-gray-100 transition duration-150">
                                 <img src="https://avatars0.githubusercontent.com/u/35900628?v=4" alt="" class="rounded-full h-6 shadow-md inline-block mr-2"/>
                                     Added new profile picture
                                     <span class="text-gray-500 text-xs">42 min ago</span>
