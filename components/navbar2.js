6c47714d4c04d470d9d98d8d630c6c5fbdb77dbb
g4urav <gaurav.gb.bahuguna@gmail.com>
2023-04-03 13:51:24 +0530

    sticky navbar

--- components/navbar2.js
+++ components/navbar2.js
@@ -15,8 +15,8 @@ function Navbar({ user,length }) {
   return (
 
     <>
-      <nav className=" border-gray-200 px-2 sm:px-4 py-2.5  bg-gray-900">
-        <div className="container flex flex-wrap items-center justify-between mx-auto">
+      <nav className=" border-gray-200 px-2 sm:px-4 py-2.5  bg-gray-900 sticky  top-0 z-50">
+        <div className="container flex flex-wrap items-center justify-between mx-auto relative">
 
           <div className="flex">
           <a href="/" className="flex items-center">
