c389395da805dc706a0f135c72bec27eb4f6a07a
darkillusior <mysteryuniverse12@gmail.com>
2023-04-08 19:28:10 +0530

    mongo

--- utilsServer/connectDb.js
+++ utilsServer/connectDb.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 async function connectDb() {
   try {
-    console.log("working man")
+    mongoose.set("strictQuery", false);
     mongoose.connect(process.env.MONGO_URI);
     console.log("Mongodb connected");
   } catch (error) {
-    console.log("gg")
+ 
     console.log(error);
     process.exit(1);
   }
