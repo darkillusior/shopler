b02e2514ef494bae664ac74cc87f3efb20fa0c0a
darkillusior <mysteryuniverse12@gmail.com>
2023-04-09 13:35:29 +0530

    l

--- utilsServer/connectDb.js
+++ utilsServer/connectDb.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 async function connectDb() {
   try {
     mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.MONGO_URI);
+    const options = {
+      serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds 
+      family: 4 // Use IPv4, skip trying IPv6
+    };
+    mongoose.connect(process.env.MONGO_URI,options);
     console.log("Mongodb connected");
   } catch (error) {
  
