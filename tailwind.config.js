b3d7dd20447f71583c5ad00aad123235f513fc16
g4urav <gaurav.gb.bahuguna@gmail.com>
2023-05-06 22:14:23 +0530

    configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-  },
+
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {
