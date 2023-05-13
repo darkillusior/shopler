d171f1a8063a0ee231f6a25513bd57667b2c903e
g4urav <gaurav.gb.bahuguna@gmail.com>
2023-05-13 21:48:02 +0530

    tailswind added again

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+
+"./components/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {
+      keyframes: {
+        popup: {
+          '0%': {
+            height:'0px'
+          },
+          '100%': {
+            height: '128px'
+       },
+        }
+       
+          
+        
+      },   
+      animation:{
+        popup:'popup 0.5s ',
+        popdown:'popdown 0.5s'
+      },
+      
+    },
+  },
+  plugins: [],
+}
