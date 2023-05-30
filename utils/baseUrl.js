29969e35a6602510b4923533925d347d8e416f9f
g4urav <gaurav.gb.bahuguna@gmail.com>
2023-05-30 12:51:14 +0530

    updated

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    :"https://shopler-q3y4h.ondigitalocean.app/login"
+    :"https://shopler-q3y4h.ondigitalocean.app"
 
 module.exports = baseUrl;
