348ed2779278ad5322874943f9303117afe20fab
g4urav <gaurav.gb.bahuguna@gmail.com>
2023-05-30 12:43:29 +0530

    baseURL digital ocean

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-   :"https://shopler.onrender.com"
+    :"https://shopler-q3y4h.ondigitalocean.app/login"
 
 module.exports = baseUrl;
